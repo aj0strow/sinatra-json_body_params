85f97612112559e0ad7f2b3263d5731a949d2f6a
aj0strow <alexander.ostrow@gmail.com>
2013-12-06 11:31:37 -0500

    more semantic

--- json_body_params.rb
+++ json_body_params.rb
@@ -2,19 +2,18 @@ require 'sinatra/base'
 require 'multi_json'
 
 module Sinatra
-  module BodyData
+  module JsonBodyParams
     
     def self.registered(app)
       app.before do
-        params.merge! body_data
+        params.merge! json_body_params
       end
       
       app.helpers do
-        def body_data
+        def json_body_params
           unless @body_data
             begin
               str = request.body.read.to_s
-              puts str
               @body_data = MultiJson.load(str, symbolize_keys: true)
             rescue MultiJson::LoadError
               @body_data = {}
